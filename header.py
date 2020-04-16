8e112ef4b083d95d3aadfcf59131947677f072e2
chenzhao <chyiru@sjtu.edu.cn>
2020-04-16 13:27:43 +0800

    configuration file

---
+++ header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: chenzhao
+
+The header file:
+    some configuration of the simulation data
+"""
+import numpy as np
+
+mapsize = 3.5*np.pi/180 ## the physcial size of simulation box
+L = 1024  ## the grid number of simulation
